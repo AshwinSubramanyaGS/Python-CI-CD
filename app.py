27ee0bd39d9a0cb7998d82c384561b66788b04b1
AshwinSubramanyaGS <ashwin.subramanya@gmail.com>
2025-09-09 16:10:25 +0530

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello V-17!!!Python 2nd chance"
+    return "Hello V-17!!!Python 3rd chance"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
